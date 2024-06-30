12b78844138661748939acaa2d9e2d9fb0566c0b
Yves Rego <ypcrego@gmail.com>
2024-06-29 23:30:53 -0300

    Testing changes

--- src/app/(tabs)/Eventos/index.tsx
+++ src/app/(tabs)/Eventos/index.tsx
@@ -1,83 +1,232 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Button, ScrollView, StyleSheet } from 'react-native';
-import { Link } from 'expo-router'
-
-interface EventProps {
-  title: string;
-  date: string;
-  status: string;
-}
-
-const Event: React.FC<EventProps> = ({ title, date, status }) => {
-  return (
-    <View style={styles.event}>
-      <Text style={styles.eventTitle}>{title}</Text>
-      <Text style={styles.eventDate}>{date}</Text>
-      <Button title={status} onPress={() => {}} color="#ff4500" />
-    </View>
-  );
-};
+import { View, Text, Pressable, StyleSheet, FlatList, TextInput, ScrollView } from 'react-native';
+import { Image } from 'expo-image';
+import React, { useEffect, useState } from 'react';
+import { router } from 'expo-router';
+import { useUserData } from '../../../contexts/AuthContext';
+import Api from '../../../services/Api';  // Usando a configuração do Api personalizada
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Eventos() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.sectionTitle}>Meus eventos</Text>
-      <View style={styles.eventBox}>
-        <Text style={styles.noEventText}>Você ainda não está participando de nenhum evento. :(</Text>
-        <Button title="Encontrar eventos" onPress={() => {}} color="#ff4500"/>
-      </View>
-      <Text style={styles.sectionTitle}>Próximos eventos</Text>
-      <ScrollView style={styles.eventsList}>
-        {/* Map your events data to these Event components */}
-        <Event title="Neon Dynasty Championship" date="22/11/2023" status="Participar" />
-        <Event title="Championship ONE PIECE 2022" date="05/04/2023" status="Em progresso" />
-        <Event title="Championship 2024 ONE PIECE" date="23/11/2024" status="Em progresso" />
-      </ScrollView>
+    const { dataUser, collectData } = useUserData(); 
+    const [playerEvents, setPlayerEvents] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [selectedTags, setSelectedTags] = useState([]);
+
+    useEffect(() => {
+        collectData();
+    }, []);
+
+    useEffect(() => {
+        if (dataUser && dataUser.role === "ROLE_PLAYER") {
+            fetchPlayerEvents();
+        }
+    }, [dataUser]); 
+
+    const fetchPlayerEvents = async () => {  
+        try {
+            const token = await AsyncStorage.getItem('token');  // Recuperando o token
+            const idUser = await AsyncStorage.getItem('userId');
+            const response = await Api.get(`api/players/${idUser}/events`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            setPlayerEvents(response.data);
+        } catch (error) {
+            console.error('Error fetching player events:', error);
+        }
+    };
+
+    const handlePress = (id: string) => {
+        router.push({
+          pathname: '/EventoX',
+          params: { id },
+        });
+    };
+
+    const handleSearch = (query: string) => {
+        setSearchQuery(query);
+    };
+
+    // TODO remove or implement
+    /*const toggleTag = (tag: string) => {
+        setSelectedTags(prevTags => 
+            prevTags.includes(tag) ? prevTags.filter(t => t !== tag) : [...prevTags, tag]
+        );
+    };*/
+
+    const filterEvents = () => {
+        let filteredEvents = playerEvents;
+        if (searchQuery) {
+            filteredEvents = filteredEvents.filter(event =>
+                event.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+                event.tags.some(tag => tag.toLowerCase().includes(searchQuery.toLowerCase()))
+            );
+        }
+    // TODO remove or implement
+       /* if (selectedTags.length > 0) {
+            filteredEvents = filteredEvents.filter(event => 
+                selectedTags.every(tag => event.tags.includes(tag))
+            );
+        }*/
+        return filteredEvents;
+    };
+
+    const renderEventCard = ({ item }) => (
+        <Pressable style={styles.eventCard} onPress={() => handlePress(item.id)}>
+            <Image style={styles.image} source={{ uri: item.imagePath }} />
+            <View style={styles.cardContent}>
+                <Text style={styles.eventTitle}>{item.name}</Text>
+                <View style={styles.tagsContainer}>
+                    {item.tags && item.tags.slice(0, 2).map((tag, index) => (
+                        <Text key={index} style={styles.tag}>{tag}</Text>
+                    ))}
+                </View>
+            </View>
+        </Pressable>
+    );
+
+ return (
+    <View style={{ flex: 1, backgroundColor: '#2D3841' }}>
+        <Text style={styles.title}>Meus Eventos</Text>
+        <TextInput
+            style={styles.searchInput}
+            placeholder="Buscar evento"
+            placeholderTextColor="#A9A9A9"
+            value={searchQuery}
+            onChangeText={handleSearch}
+        />
+        {/** TODO REMOVE OR IMPLEMENT */}
+        {/**<ScrollView horizontal style={styles.tagFilterContainer}>
+            /** Substitua as tags abaixo com as tags reais que você tem
+            {['Tag1', 'Tag2', 'Tag3'].map((tag, index) => (
+                <Pressable
+                    key={index}
+                    style={[
+                        styles.tagButton,
+                        selectedTags.includes(tag) && styles.selectedTag
+                    ]}
+                    onPress={() => toggleTag(tag)}
+                >
+                    <Text style={styles.tagButtonText}>{tag}</Text>
+                </Pressable>
+            ))}
+        </ScrollView>*/}
+        {dataUser && dataUser.role === "ROLE_PLAYER" && filterEvents().length === 0 ? (
+            <View style={[styles.noEventsContainer, { justifyContent: 'center', alignItems: 'center'  }]}>
+                <Text style={[styles.noEventsText, { textAlign: 'center' }]}>
+                    Nenhum evento foi encontrado com os parâmetros definidos!
+                </Text>
+            </View>
+        ) : (
+            <FlatList
+                data={filterEvents()}
+                renderItem={renderEventCard}
+                keyExtractor={item => item.id}
+                contentContainerStyle={styles.flatListContent}
+                numColumns={2}
+                showsVerticalScrollIndicator={false}
+            />
+        )}
     </View>
-  )
+);
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      backgroundColor: 'white',
+    title: {
+        marginTop: 20,
+        marginLeft: 10,
+        color: '#FFFFFF',
+        fontSize: 24,
+        fontWeight: 'bold',
     },
-    banner: {
-      width: '100%',
-      height: 200,
-      resizeMode: 'cover'
+    searchInput: {
+        backgroundColor: '#364753',
+        padding: 10,
+        borderRadius: 15,
+        margin: 10,
+        color: '#FFFFFF',
+        fontSize: 16,
     },
-    sectionTitle: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginTop: 20,
-      marginLeft: 10,
+    tagFilterContainer: {
+        marginLeft: 10,
+        marginBottom: 10,
     },
-    eventBox: {
-      alignItems: 'center',
-      marginVertical: 20,
+    tagButton: {
+        backgroundColor: '#364753',
+        padding: 10,
+        borderRadius: 15,
+        marginRight: 10,
     },
-    noEventText: {
-      fontSize: 16,
-      color: 'grey',
-      marginBottom: 10,
+    selectedTag: {
+        backgroundColor: '#FF6347',
     },
-    eventsList: {
-      paddingHorizontal: 10,
+    tagButtonText: {
+        color: '#FFFFFF',
+        fontSize: 14,
     },
-    event: {
-      backgroundColor: '#f0f0f0',
-      padding: 10,
-      marginBottom: 10,
-      borderRadius: 5,
+    noEventsContainer: {
+        borderRadius: 15,
+        backgroundColor: '#364753',
+        width: '100%',
+        height: 150,
+        marginBottom: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    noEventsText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        marginBottom: 10,
+    },
+    findEventsButton: {
+        backgroundColor: 'blue',
+        padding: 10,
+        alignItems: 'center',
+        borderRadius: 15,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+    },
+    flatListContent: {
+        paddingHorizontal: 10,
+    },
+    eventCard: {
+        flex: 1,
+        backgroundColor: '#364753',
+        borderRadius: 15,
+        margin: 5,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '45%',
+    },
+    image: {
+        width: '100%',
+        height: 100,
+        borderRadius: 15,
+    },
+    cardContent: {
+        marginTop: 10,
+        alignItems: 'center',
     },
     eventTitle: {
-      fontSize: 16,
-      fontWeight: 'bold',
+        color: '#FFFFFF',
+        fontSize: 18,
+        fontWeight: 'bold',
     },
-    eventDate: {
-      fontSize: 14,
-      color: 'grey',
-    }
-  });
+    tagsContainer: {
+        flexDirection: 'row',
+        marginTop: 5,
+    },
+    tag: {
+        backgroundColor: '#FF6347',
+        color: '#FFFFFF',
+        padding: 5,
+        borderRadius: 5,
+        marginRight: 5,
+        fontSize: 12,
+    },
+});
--- src/app/EventoX/index.tsx
+++ src/app/EventoX/index.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text, Pressable, FlatList} from "react-native";
+import { View, StyleSheet, Text, Pressable, FlatList, Alert } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import { Fontisto } from '@expo/vector-icons';
 import { Image } from 'expo-image';
@@ -8,126 +8,192 @@ import { useUserData } from "../../contexts/AuthContext";
 import { useLocalSearchParams } from 'expo-router';
 import { useUserEventData } from "../../contexts/EventContext";
 import { Feather } from '@expo/vector-icons';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function EventoX(){
+export default function EventoX() {
   const { dataUser, collectData, Renderize } = useUserData();
-  const {event, collectEventDataById} = useUserEventData();
+  const { event, collectEventDataById } = useUserEventData();
   const [selectedButton, setSelectedButton] = useState(null);
   const handleButtonPress = (buttonIndex) => {
     setSelectedButton(buttonIndex);
   };
 
-    const { id } = useLocalSearchParams();
-  
+  const { id } = useLocalSearchParams();
+  const [userRole, setUserRole] = useState('');
+  const [isUserSubscribed, setIsUserSubscribed] = useState(false); // Estado para verificar se o usuário está inscrito
 
-    useEffect(() => {
-      if (id) {
-        collectEventDataById(id as string);
-        console.log("aaaa");
+
+  // TODO check uso de async storage - role
+  useEffect(() => {
+    async function fetchUserRole() {
+      const role = await AsyncStorage.getItem('userRole');
+      console.log("roleee: " + role)
+      setUserRole(role);
+    }
+
+    async function checkUserSubscription() {
+      const userId = await AsyncStorage.getItem('userId');
+      // Lógica para verificar se o usuário está inscrito no evento (substitua pelo seu método de verificação)
+      const userIsSubscribed = await checkUserIsSubscribedToEvent(userId, event.id);
+      setIsUserSubscribed(userIsSubscribed);
+    }
+
+    if (id) {
+      collectEventDataById(id as string);
+      console.log("aaaa");
+    }
+    fetchUserRole();
+    checkUserSubscription();
+
+  }, [id]); // executa apenas quando id mudar
+
+  const handleInscricao = async () => {
+    try {
+      const userId = await AsyncStorage.getItem('userId');
+      const token = await AsyncStorage.getItem('token'); // Obter token JWT do AsyncStorage
+
+      const response = await fetch(`api/users/${userId}/events/add`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`, // Incluir token JWT no cabeçalho Authorization
+        },
+        body: JSON.stringify({
+          eventId: event.id, // Supondo que event.id é o ID do evento atual
+        }),
+      });
+
+      if (response.ok) {
+        // Tratar a resposta conforme necessário
+        Alert.alert('Inscrição realizada com sucesso!');
+      } else {
+        // Tratar erros de requisição
+        Alert.alert('Erro ao se inscrever no evento.');
       }
-    }, [id]); // executa apenas quando id mudar
- 
+    } catch (error) {
+      console.error('Erro ao se inscrever no evento:', error);
+      Alert.alert('Erro ao se inscrever no evento.');
+    }
+  };
+  
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.myevents}>
+        <Image style={styles.imageContainer} source={event.imagePath}></Image>
 
-    return(
-        <View style={styles.mainContainer}>
-            <View style={styles.myevents}> 
-              <Image style={styles.imageContainer} source={event.imagePath}></Image>
-            
-               <View style={styles.titleContainer}>
-                  <Text style={styles.titleEventName}> {event.name} </Text>  
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleEventName}> {event.name} </Text>
 
-                  <View style={styles.infoCardsContainer}>
-                
-                    <View style={styles.cardPlayersNumber}>
-                      < Ionicons name="people" size={28} color="#9747FF" />
-                      <Text style={styles.titlePlayersNumber}> 35</Text>
-                    </View>
-                    <View style={styles.cardEventDate}>
-                      <Fontisto name="date" size={24} color="#4ECB71" />
-                      <Text style={styles.titleEventDate}> 02/05/24</Text>
-                    </View>
-                  </View>
-                
-                  <Pressable style={styles.buttonSeeEvent}>
-                    <Text style={styles.buttonMyEventText1}>Iniciar Evento</Text>
-                  </Pressable>
-                
-              </View>
-              
+          <View style={styles.infoCardsContainer}>
+
+            <View style={styles.cardPlayersNumber}>
+              < Ionicons name="people" size={28} color="#9747FF" />
+              <Text style={styles.titlePlayersNumber}> 35</Text>
             </View>
-
-            <View style={styles.navbarcontainer} >
-                
-                  <Pressable style={styles.navbarButtons} onPress={()=> handleButtonPress(1)}>
-                    <Text style={[
-                        selectedButton === 1 && styles.selectedText
-                    ]}>Detalhes</Text>
-                  </Pressable>
-                  <Pressable style={styles.navbarButtons} onPress={()=> handleButtonPress(2)}>
-                    <Text style={[
-                        selectedButton === 2 && styles.selectedText
-                    ]}>Torneio</Text>
-                  </Pressable>
-                  <Pressable style={styles.navbarButtons} onPress={()=> handleButtonPress(3)}>
-                    <Text style={[
-                        selectedButton === 3 && styles.selectedText
-                    ]}>Rodada</Text>
-                  </Pressable>
-                  <Pressable style={styles.navbarButtons} onPress={()=> handleButtonPress(4)}>
-                    <Text style={[
-                        selectedButton === 4 && styles.selectedText
-                    ]}>Grade</Text>
-                  </Pressable>
+            <View style={styles.cardEventDate}>
+              <Fontisto name="date" size={24} color="#4ECB71" />
+              <Text style={styles.titleEventDate}> 02/05/24</Text>
             </View>
-            
-            {selectedButton == 1? (
-              <View>
-                 <View style={styles.textdetailscontainer}> 
-                  <Text style={styles.TextTitleDetalhes}>Detalhes</Text>
-                  <Feather name="edit" size={20} color="gray" />
-                 </View>
-                  <Text style={styles.TextDetails}> {event.description}</Text>
-                 <View style={styles.textdetailscontainer}> 
-                  <Text style={styles.TextTitleDetalhes}>Cronograma</Text>
-                  <Feather name="edit" size={20} color="gray" />
-                 </View>
-                 <Text style={styles.TextDetails}> {event.description}</Text>
-                 <View style={styles.textdetailscontainer}> 
-                  <Text style={styles.TextTitleDetalhes}>Premiação</Text>
-                  <Feather name="edit" size={20} color="gray" />
-                 </View>
-                 <Text style={styles.TextDetails}> {event.description}</Text>
-                
-                
-              </View>
-              
-            ): null}
+          </View>
 
-            {selectedButton == 2? (
-              <View>
-                <View style={styles.textTitleContainer}> 
-                  <Text style={styles.TextTitleTorneio}>Torneio </Text> 
-                  
-                </View>
-               
+          <Pressable
+            style={styles.buttonSeeEvent}
+            onPress={() => {
+              if (userRole === 'ROLE_MANAGER') {
+                // Lógica para gerentes
+                console.log('Lógica para gerentes');
+              } else {
+                // Lógica para usuários normais (ROLE_PLAYER)
+                handleInscricao();
+              }
+            }}>
+            <Text style={styles.buttonMyEventText1}>
+              {userRole === 'ROLE_MANAGER' ? 'Iniciar Evento' : 'Inscrever-se'}
+            </Text>
+          </Pressable>
 
-              </View>
-            ): null}
-
-             {selectedButton == 3? (
-              <View>
-              <Text style={styles.TextTitleDetalhes}>Rodada</Text>
-            </View>
-            ): null}
-
-            {selectedButton == 4? (
-              <View>
-              <Text style={styles.TextTitleDetalhes}>Grade</Text>
-            </View>
-            ): null}
-            
         </View>
-    )
+
+      </View>
+
+      <View style={styles.navbarcontainer} >
+
+        <Pressable style={styles.navbarButtons} onPress={() => handleButtonPress(1)}>
+          <Text style={[
+            selectedButton === 1 && styles.selectedText
+          ]}>Detalhes</Text>
+        </Pressable>
+        <Pressable style={styles.navbarButtons} onPress={() => handleButtonPress(2)}>
+          <Text style={[
+            selectedButton === 2 && styles.selectedText
+          ]}>Torneio</Text>
+        </Pressable>
+        <Pressable style={styles.navbarButtons} onPress={() => handleButtonPress(3)}>
+          <Text style={[
+            selectedButton === 3 && styles.selectedText
+          ]}>Rodada</Text>
+        </Pressable>
+        <Pressable style={styles.navbarButtons} onPress={() => handleButtonPress(4)}>
+          <Text style={[
+            selectedButton === 4 && styles.selectedText
+          ]}>Grade</Text>
+        </Pressable>
+      </View>
+
+      {selectedButton == 1 ? (
+        <View>
+          <View style={styles.textdetailscontainer}>
+          <Text style={styles.TextTitleDetalhes}>Detalhes</Text>
+            {userRole === 'ROLE_MANAGER' && (
+              <Feather name="edit" size={20} color="gray" />
+            )}
+          </View>
+          <Text style={styles.TextDetails}> {event.description}</Text>
+          <View style={styles.textdetailscontainer}>
+          <Text style={styles.TextTitleDetalhes}>Cronograma</Text>
+            {userRole === 'ROLE_MANAGER' && (
+              <Feather name="edit" size={20} color="gray" />
+            )}
+          </View>
+          <Text style={styles.TextDetails}> {event.description}</Text>
+          <View style={styles.textdetailscontainer}>
+          <Text style={styles.TextTitleDetalhes}>Premiação</Text>
+            {userRole === 'ROLE_MANAGER' && (
+              <Feather name="edit" size={20} color="gray" />
+            )}
+          </View>
+          <Text style={styles.TextDetails}> {event.description}</Text>
+
+
+        </View>
+
+      ) : null}
+
+      {selectedButton == 2 ? (
+        <View>
+          <View style={styles.textTitleContainer}>
+            <Text style={styles.TextTitleTorneio}>Torneio </Text>
+
+          </View>
+
+
+        </View>
+      ) : null}
+
+      {selectedButton == 3 ? (
+        <View>
+          <Text style={styles.TextTitleDetalhes}>Rodada</Text>
+        </View>
+      ) : null}
+
+      {selectedButton == 4 ? (
+        <View>
+          <Text style={styles.TextTitleDetalhes}>Grade</Text>
+        </View>
+      ) : null}
+
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -139,7 +205,7 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
 
-  TextTitleTorneio:{
+  TextTitleTorneio: {
     color: 'white',
     fontSize: 24,
     margin: 20,
@@ -156,7 +222,7 @@ const styles = StyleSheet.create({
     fontSize: 12,
     marginHorizontal: 30
   },
-  TextTitleDetalhes:{
+  TextTitleDetalhes: {
     color: 'white',
     fontSize: 24,
     margin: 20
@@ -173,115 +239,115 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: '25%',
     justifyContent: 'center'
-    
+
   }
   ,
-   navbarcontainer: {
+  navbarcontainer: {
     flexDirection: 'row',
     width: '100%',
     height: 35,
-    
+
     justifyContent: 'space-between',
-  
-   }
+
+  }
   ,
-    mainContainer: {
-        backgroundColor: '#364753',
-        flex: 1,
-    },
-    myevents: {
-        flexDirection: 'row',
-        borderRadius: 15,
-        backgroundColor: '#364753',
-        width: 400,
-        height: 150,
-        marginBottom: 10,
-        
-      },
-      buttonSearchEvents: {
-        backgroundColor: 'green',
-        width: '80%',
-        padding: 20,
-        margin: 20,
-        alignItems: 'center',
-        borderRadius: 15
-      },
-      titleMyEvents: {
-        marginTop: 20,
-        color: '#FFFFFF',
-        fontSize: 24,
-        
-      },
-      myEventsDescription: {
-        color: '#FFFFFF',
-        fontSize: 16,
-        marginBottom: 10
-      },
-      buttonMyEventText: {
-        color: '#FFFFFF',
-        fontSize: 20
-      },
-      imageContainer: {
-       padding: 70,
-      },
-      titleContainer: {
-        flex: 1,
-        marginRight: 20,
-        marginLeft: 10,
-        marginTop: 10,
-        alignItems: 'center'
-      },
-      titleEventName: {
-        color: '#FFFFFF',
-        fontSize: 20,
-      },
-      infoCardsContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        gap: -25
-      },
-      cardPlayersNumber: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginLeft: 10,
-        width: 5,
-        height: 30,
-        gap: 5
-      },
-      titlePlayersNumber: {
-        color: '#9747FF',
-        fontWeight: 'bold'
-      },
-      cardEventDate: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        width: 5,
-        height: 30,
-        gap: 5
-      },
-      titleEventDate: {
-        color: '#4ECB71',
-        fontWeight: 'bold'
-      },
-      buttonSeeEvent: {
-        flex: 1,
-        backgroundColor: '#364753',
-        width: '80%',
-        flexDirection: 'row',
-        margin: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 15,
-        borderColor: 'green',
-        borderWidth: 2
-      },
-    
-      
-      buttonMyEventText1: {
-        color: '#FFFFFF',
-        fontSize: 20
-      },
+  mainContainer: {
+    backgroundColor: '#364753',
+    flex: 1,
+  },
+  myevents: {
+    flexDirection: 'row',
+    borderRadius: 15,
+    backgroundColor: '#364753',
+    width: 400,
+    height: 150,
+    marginBottom: 10,
+
+  },
+  buttonSearchEvents: {
+    backgroundColor: 'green',
+    width: '80%',
+    padding: 20,
+    margin: 20,
+    alignItems: 'center',
+    borderRadius: 15
+  },
+  titleMyEvents: {
+    marginTop: 20,
+    color: '#FFFFFF',
+    fontSize: 24,
+
+  },
+  myEventsDescription: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    marginBottom: 10
+  },
+  buttonMyEventText: {
+    color: '#FFFFFF',
+    fontSize: 20
+  },
+  imageContainer: {
+    padding: 70,
+  },
+  titleContainer: {
+    flex: 1,
+    marginRight: 20,
+    marginLeft: 10,
+    marginTop: 10,
+    alignItems: 'center'
+  },
+  titleEventName: {
+    color: '#FFFFFF',
+    fontSize: 20,
+  },
+  infoCardsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    gap: -25
+  },
+  cardPlayersNumber: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 10,
+    width: 5,
+    height: 30,
+    gap: 5
+  },
+  titlePlayersNumber: {
+    color: '#9747FF',
+    fontWeight: 'bold'
+  },
+  cardEventDate: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: 5,
+    height: 30,
+    gap: 5
+  },
+  titleEventDate: {
+    color: '#4ECB71',
+    fontWeight: 'bold'
+  },
+  buttonSeeEvent: {
+    flex: 1,
+    backgroundColor: '#364753',
+    width: '80%',
+    flexDirection: 'row',
+    margin: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    borderColor: 'green',
+    borderWidth: 2
+  },
+
+
+  buttonMyEventText1: {
+    color: '#FFFFFF',
+    fontSize: 20
+  },
 
 })
